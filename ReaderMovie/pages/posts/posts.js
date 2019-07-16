13051f2a0327667b546e8d8db1dba92ec55ac950
王嘉巍 <906789088@qq.com>
2019-07-16 11:53:57 +0800

    data bind in  onLoad collect data only submit data in js and wxml

--- ReaderMovie/pages/posts/posts.js
+++ ReaderMovie/pages/posts/posts.js
@@ -4,14 +4,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    date:"Sep 15 2019",
+    title:"VueConf in Shanghai"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var post_content1 = {
+      date:"Sep 15 2019",
+      title:"VueConf in Shanghai",
+      post_img: "../../images/post/me.jpg",
+      content: "VueConf 2019 成功在上海交通大学徐汇校区举办，Vue作者尤雨溪亲自到达现场，和各位开发者们一起分享了Vue3.0的进展，并在会后和大家签名合影留念",
+      view_num: "92",
+      collect_num: "65",
+      author_img: "../../images/avatar/1.jpg"
+    }
+    this.setData(post_content1)
   },
 
   /**
