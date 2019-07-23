061e764058c43fff7a042773dde520aa623bc1a3
王嘉巍 <906789088@qq.com>
2019-07-23 16:48:59 +0800

    left bug for this postData in post-detail and dont't change this page

--- ReaderMovie/data/post-data.js
+++ ReaderMovie/data/post-data.js
@@ -7,6 +7,10 @@ var local_database = [
     content: "VueConf 2019 成功在上海交通大学徐汇校区举办，Vue作者尤雨溪亲自到达现场，和各位开发者们一起分享了Vue3.0的进展，并在会后和大家签名合影留念",
     reading: "92",
     collection: "65",
+    headImgSrc: "/images/post/withyyx.jpg",
+    author: "Anthony",
+    dateTime: "24小时之前",
+    detail: "由Vue.js官方联合w3ctech、前端圈联合举办的第三届VueConf 定于2019年6月8日在上海举办。本次大会邀请了Vue.js作者尤雨溪、Vue.js官方团队成员蒋豪群、快手前端架构师天翔、汇丰科技中国 投资银行部 首席技术顾问卢俊杰、Vue.js 团队成员，VuePress维护者，蚂蚁金服高级前端工程师真山等出席本次大会，并发表主题演讲。",
     postId: 0
   }, {
     date:"June 26 2019",
@@ -16,6 +20,10 @@ var local_database = [
     content: "2019年是中国人民共产党成立70周年，10月1日将在天安门前举行盛大的阅兵仪式，届时我国新型武器将会亮相，会深受世界人民的关注",
     reading: "112",
     collection: "78",
+    headImgSrc: "/images/post/tianandoor.jpg",
+    author: "苏苏小姐姐",
+    dateTime: "一周前",
+    detail: "10月1日，将在天安门前举行盛大的阅兵仪式。届时我国新型武器将会亮相，会深受世界人民的关注,我国每年都保持着10年一大阅，5年一小阅的传统，届时全世界的眼球再次聚在中国",
     postId: 1
   }, {
     date:"Jan 7 2019",
@@ -25,6 +33,10 @@ var local_database = [
     content: "2019年当我买到精通CSS这本书之后，那天晚上心情很愉快，就拍了我都书桌，觉得很有一个仪式感，为新书的进入也算吗起了个好的开头",
     reading: "201",
     collection: "138",
+    headImgSrc: "/images/post/computer.jpg",
+    author: "Lebron James",
+    dateTime: "刚刚",
+    detail: "本书是CSS设计经典图书升级版，结合CSS近年来的发展，尤其是CSS3和HTML5的特性，对内容进行了全面改写。本书介绍了涉及字体、网页布局、响应式Web设计、表单、动画等方面的实用技巧，并讨论了如何实现稳健、灵活、无障碍访问的Web设计，以及在技术层面如何实现跨浏览器方案和后备方案。本书还介绍了一些鲜为人知的高级技巧，让你的Web设计脱颖而出。",
     postId: 2
   }
 ]
--- ReaderMovie/pages/posts/post-detail/post-detail.js
+++ ReaderMovie/pages/posts/post-detail/post-detail.js
@@ -1,3 +1,8 @@
+var postsData = require('../../../data/post-data')
 Page({
-  
+  onLoad: function(option) {
+    var postId = option.id;
+    var postData = postsData.postList[postId];
+    this.data.postData = postData;
+  }
 })
