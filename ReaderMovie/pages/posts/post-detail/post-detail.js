1d6a99016fb31ea9038885545ee4c7d411073aba
王嘉巍 <906789088@qq.com>
2019-07-25 11:35:06 +0800

    setStorage、getStorage function and using removeStorage to clear

--- ReaderMovie/pages/posts/post-detail/post-detail.js
+++ ReaderMovie/pages/posts/post-detail/post-detail.js
@@ -4,5 +4,15 @@ Page({
     var postId = option.id;
     var postData = postsData.postList[postId];
     this.setData(postData);
+    wx.setStorageSync('key', {
+      game: "风暴",
+      developer: "暴雪"
+    })
+  },
+  onCollectionTap: function(event) {
+    var game = wx.getStorageSync('key')
+  },
+  onShareTap: function(event) {
+    wx.removeStorageSync('key')
   }
 })
