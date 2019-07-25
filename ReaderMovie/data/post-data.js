660aab950440afa26dfb28ce95bea7355dfa1d01
王嘉巍 <906789088@qq.com>
2019-07-25 10:29:00 +0800

    fix bug for img don't display this avatar in post-detail

--- ReaderMovie/data/post-data.js
+++ ReaderMovie/data/post-data.js
@@ -2,8 +2,8 @@ var local_database = [
   {
     date:"Sep 15 2019",
     title:"VueConf in Shanghai",
-    imgSrc: "../../images/post/me.jpg",
-    avatar: "../../images/avatar/1.jpg",
+    imgSrc: "/images/post/me.jpg",
+    avatar: "/images/avatar/1.jpg",
     content: "VueConf 2019 成功在上海交通大学徐汇校区举办，Vue作者尤雨溪亲自到达现场，和各位开发者们一起分享了Vue3.0的进展，并在会后和大家签名合影留念",
     reading: "92",
     collection: "65",
@@ -15,8 +15,8 @@ var local_database = [
   }, {
     date:"June 26 2019",
     title:"Beijing 2019",
-    imgSrc: "../../images/post/tianandoor.jpg",
-    avatar: "../../images/avatar/2.jpg",
+    imgSrc: "/images/post/tianandoor.jpg",
+    avatar: "/images/avatar/2.jpg",
     content: "2019年是中国人民共产党成立70周年，10月1日将在天安门前举行盛大的阅兵仪式，届时我国新型武器将会亮相，会深受世界人民的关注",
     reading: "112",
     collection: "78",
@@ -28,8 +28,8 @@ var local_database = [
   }, {
     date:"Jan 7 2019",
     title: "My Computer World",
-    imgSrc: "../../images/post/computer.jpg",
-    avatar: "../../images/avatar/4.jpg",
+    imgSrc: "/images/post/computer.jpg",
+    avatar: "/images/avatar/4.jpg",
     content: "2019年当我买到精通CSS这本书之后，那天晚上心情很愉快，就拍了我都书桌，觉得很有一个仪式感，为新书的进入也算吗起了个好的开头",
     reading: "201",
     collection: "138",
