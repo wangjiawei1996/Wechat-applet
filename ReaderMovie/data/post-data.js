64a7d86ae7d1280c403e1bf005496a6b12a346b3
王嘉巍 <906789088@qq.com>
2019-07-30 17:07:24 +0800

    fix bug for music play and import active data in music

--- ReaderMovie/data/post-data.js
+++ ReaderMovie/data/post-data.js
@@ -11,7 +11,12 @@ var local_database = [
     author: "Anthony",
     dateTime: "24小时之前",
     detail: "由Vue.js官方联合w3ctech、前端圈联合举办的第三届VueConf 定于2019年6月8日在上海举办。本次大会邀请了Vue.js作者尤雨溪、Vue.js官方团队成员蒋豪群、快手前端架构师天翔、汇丰科技中国 投资银行部 首席技术顾问卢俊杰、Vue.js 团队成员，VuePress维护者，蚂蚁金服高级前端工程师真山等出席本次大会，并发表主题演讲。",
-    postId: 0
+    postId: 0,
+    music: {
+      url: 'http://www.170mv.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_56736979&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
+      title: 'PopStar',
+      coverImgUrl: 'https://image.baidu.com/search/detail?ct=503316480&z=0&ipn=d&word=popstar%E8%8B%B1%E9%9B%84%E8%81%94%E7%9B%9F&step_word=&hs=0&pn=8&spn=0&di=25080&pi=0&rn=1&tn=baiduimagedetail&is=0%2C0&istype=0&ie=utf-8&oe=utf-8&in=&cl=2&lm=-1&st=undefined&cs=4290013275%2C3600974309&os=1541071257%2C3706878731&simid=4234419772%2C680214104&adpicid=0&lpn=0&ln=225&fr=&fmq=1564394789127_R&fm=&ic=undefined&s=undefined&hd=undefined&latest=undefined&copyright=undefined&se=&sme=&tab=0&width=undefined&height=undefined&face=undefined&ist=&jit=&cg=&bdtype=0&oriquery=&objurl=http%3A%2F%2Fp2.music.126.net%2FK9Ush0UgQ4sKQApHBa8r1A%3D%3D%2F109951163790600768.jpg&fromurl=ippr_z2C%24qAzdH3FAzdH3F47ftv_z%26e3B8mn_z%26e3Bv54AzdH3Ff5g2%3Ft1%3D8nnln9mllm%267fj6t1%3D9bm9d00ma%26u654%3Dpt4jstgj&gsm=0&rpstart=0&rpnum=0&islist=&querylist=&force=undefined'
+    }
   }, {
     date:"June 26 2019",
     title:"Beijing 2019",
@@ -24,7 +29,12 @@ var local_database = [
     author: "苏苏小姐姐",
     dateTime: "一周前",
     detail: "10月1日，将在天安门前举行盛大的阅兵仪式。届时我国新型武器将会亮相，会深受世界人民的关注,我国每年都保持着10年一大阅，5年一小阅的传统，届时全世界的眼球再次聚在中国",
-    postId: 1
+    postId: 1,
+    music: {
+      url: 'http://www.170mv.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_56736979&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
+      title: "夜夜夜夜-齐秦",
+      coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
+    }
   }, {
     date:"Jan 7 2019",
     title: "My Computer World",
@@ -37,7 +47,12 @@ var local_database = [
     author: "Lebron James",
     dateTime: "刚刚",
     detail: "本书是CSS设计经典图书升级版，结合CSS近年来的发展，尤其是CSS3和HTML5的特性，对内容进行了全面改写。本书介绍了涉及字体、网页布局、响应式Web设计、表单、动画等方面的实用技巧，并讨论了如何实现稳健、灵活、无障碍访问的Web设计，以及在技术层面如何实现跨浏览器方案和后备方案。本书还介绍了一些鲜为人知的高级技巧，让你的Web设计脱颖而出。",
-    postId: 2
+    postId: 2,
+    music: {
+      url: 'http://www.170mv.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_56736979&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
+      title: "恋恋风尘-老狼",
+      coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001VaXQX1Z1Imq.jpg?max_age=2592000",
+     }
   }
 ]
 module.exports = {
