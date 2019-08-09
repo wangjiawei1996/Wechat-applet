5e04cdb2db44b4563a5e685654544fabe01beffe
王嘉巍 <906789088@qq.com>
2019-08-09 22:09:23 +0800

    interface in movies and create url and header success

--- ReaderMovie/pages/movies/movies.js
+++ ReaderMovie/pages/movies/movies.js
@@ -1,3 +1,13 @@
 Page({
-  
+  onLoad: function(event) {
+    wx.request({
+      url: 'http://v.juhe.cn/movie/index',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        console.log(res)
+      }
+    })
+  }
 })
