f1eb6f6b13fea8373c19d6b8976d2a7926e8c7e0
王嘉巍 <906789088@qq.com>
2019-08-20 20:00:38 +0800

    add globalData url in getMovieListData in event

--- ReaderMovie/pages/movies/movies.js
+++ ReaderMovie/pages/movies/movies.js
@@ -1,7 +1,16 @@
+var app = getApp();
 Page({
   onLoad: function(event) {
+    var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters";
+    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon";
+    var top250Url = app.globalData.doubanBase + "/v2/movie/top250";
+    this.getMovieListData(inTheatersUrl);
+    this.getMovieListData(comingSoonUrl);
+    this.getMovieListData(top250Url);
+  },
+  getMovieListData: function(url) {
     wx.request({
-      url: 'https://douban.uieee.com/v2/movie/top250',
+      url: url,
       method: 'GET',
       header: {
         'Content-Type': 'application/xml'
