1cbd4b60c2d8be2064d2ba5458066868641cd10a
王嘉巍 <906789088@qq.com>
2019-08-21 20:49:50 +0800

    left bug for data movies in list-template and set

--- ReaderMovie/pages/movies/movies.js
+++ ReaderMovie/pages/movies/movies.js
@@ -1,14 +1,19 @@
 var app = getApp();
 Page({
+  data: {
+    inTheaters: {},
+    commingSoon: {},
+    top250Url: {}
+  },
   onLoad: function(event) {
     var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters" + "?start=0&count=3";
     var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
     var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
-    this.getMovieListData(inTheatersUrl);
-    this.getMovieListData(comingSoonUrl);
-    this.getMovieListData(top250Url);
+    this.getMovieListData(inTheatersUrl, "inTheaters");
+    this.getMovieListData(comingSoonUrl, "commingSoon");
+    this.getMovieListData(top250Url, "top250Url");
   },
-  getMovieListData: function(url) {
+  getMovieListData: function(url, settedKey) {
     var that = this;
     wx.request({
       url: url,
@@ -18,11 +23,11 @@ Page({
       },
       success: function(res) {
         console.log(res)
-        that.processDoubanData(res.data)
+        that.processDoubanData(res.data, settedKey)
       }
     })
   },
-  processDoubanData: function(moviesDouban) {
+  processDoubanData: function(moviesDouban, settedKey) {
     var movies = [];
     for(var idx in moviesDouban.subjects) {
       var subject = moviesDouban.subjects[idx];
@@ -38,8 +43,10 @@ Page({
       }
       movies.push(temp)
     }
-    this.setData({
+    var readyData = {};
+    readyData[settedKey] = {
       movies: movies
-    })
+    };
+    this.setData(readyData);
   }
 })
