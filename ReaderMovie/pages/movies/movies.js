5759315727e851a32b0c01d6d4217536119c6847
王嘉巍 <906789088@qq.com>
2019-08-22 20:21:34 +0800

    block this stars in template and design elif in movie

--- ReaderMovie/pages/movies/movies.js
+++ ReaderMovie/pages/movies/movies.js
@@ -1,3 +1,4 @@
+var util = require('../../utils/utils')
 var app = getApp();
 Page({
   data: {
@@ -36,6 +37,7 @@ Page({
         title = title.substring(0, 6) + '...'
       }
       var temp = {
+        stars: util.convertToStarsArray(subject.rating.stars),
         title: title,
         average: subject.rating.average,
         coverageUrl: subject.images.large,
