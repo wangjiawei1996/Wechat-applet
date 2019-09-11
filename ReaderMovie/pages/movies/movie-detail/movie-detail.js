8cc05bbe085ad34f056deb9f431b62e598076106
王嘉巍 <906789088@qq.com>
2019-09-11 20:06:06 +0800

    module.exports the convertToCastString, convertToCastInfos

--- ReaderMovie/pages/movies/movie-detail/movie-detail.js
+++ ReaderMovie/pages/movies/movie-detail/movie-detail.js
@@ -2,7 +2,7 @@ var util = require('../../../utils/utils')
 var app = getApp();
 Page({
   data: {
-
+    movie: {}
   },
   onLoad: function (options) {
     var movieId = options.id;
@@ -10,6 +10,40 @@ Page({
     util.http(url, this.processDoubanData)
   },
   processDoubanData: function(data) {
-    
+    if(!data){
+      return;
+    }
+    var director = {
+        avatar: "",
+        name: "",
+        id: ""
+    }
+    if (data.directors[0] != null) {
+        if (data.directors[0].avatars != null) {
+            director.avatar = data.directors[0].avatars.large
+
+        }
+        director.name = data.directors[0].name;
+        director.id = data.directors[0].id;
+    }
+    var movie = {
+        movieImg: data.images ? data.images.large : "",
+        country: data.countries[0],
+        title: data.title,
+        originalTitle: data.original_title,
+        wishCount: data.wish_count,
+        commentCount: data.comments_count,
+        year: data.year,
+        generes: data.genres.join("、"),
+        stars: util.convertToStarsArray(data.rating.stars),
+        score: data.rating.average,
+        director: director,
+        casts: util.convertToCastString(data.casts),
+        castsInfo: util.convertToCastInfos(data.casts),
+        summary: data.summary
+    }
+    this.setData({
+      movie: movie
+    })
   }
 })
--- ReaderMovie/utils/utils.js
+++ ReaderMovie/utils/utils.js
@@ -25,8 +25,27 @@ function http(url,callBack,data,method){
     }
   })
 }
-
+function convertToCastString(casts) {
+  var castsjoin = "";
+  for (var idx in casts) {
+    castsjoin = castsjoin + casts[idx].name + " / ";
+  }
+  return castsjoin.substring(0, castsjoin.length - 2);
+}
+function convertToCastInfos(casts) {
+  var castsArray = []
+  for (var idx in casts) {
+    var cast = {
+      img: casts[idx].avatars ? casts[idx].avatars.large : "",
+      name: casts[idx].name
+    }
+    castsArray.push(cast);
+  }
+  return castsArray;
+}
 module.exports = {
-convertToStarsArray: convertToStarsArray,
-http: http
+  convertToStarsArray: convertToStarsArray,
+  http: http,
+  convertToCastString:convertToCastString,
+  convertToCastInfos:convertToCastInfos
 }
